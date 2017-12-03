5d2a9075895e4774bdb6f46b444f99d1e23bf9c0
JForbes15 <a1504533@myy.haaga-helia.fi>
2017-12-03 15:23:08 +0000

    Update idea.js

--- src/handlers/idea.js
+++ src/handlers/idea.js
@@ -2,4 +2,4 @@ import {
   dbGetIdeas,
 } from '../models/idea';
 
-export const getIdeas = (req, res) => dbGetIdeas().then(data => res.json(data));
+export const getIdeasById = (req, res) => dbGetIdeasId().then(data => res.json(data));

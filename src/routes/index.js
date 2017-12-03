2752c44aa2a75b21c7a19162fc74d6a48fb780a1
JForbes15 <a1504533@myy.haaga-helia.fi>
2017-12-03 18:42:55 +0000

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,12 +6,12 @@ import {
 } from '../handlers/idea';
 
 router.get('/api/ideas', getIdeas);
-router.get('/api/ideas:id', getIdeasById);
-router.get('/api/ideas:id/comments', getIdeasCommentById); 
+router.get('/api/ideas/:id', getIdeasById);
+router.get('/api/ideas/:id/comments', getIdeasCommentById); 
 router.get('/api/ideas', getIdeas);
 router.post('/api/ideas', postIdeas);
-router.put('/api/ideas:id', putIdeasById);
-router.delete('/api/ideas:id', deleteIdeasbById);
+router.put('/api/ideas/:id', putIdeasById);
+router.delete('/api/ideas/:id', deleteIdeasbById);
 
 
 module.exports = router;

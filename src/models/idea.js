242b6a4bebdee33d036143be8382e45eb7147e46
JForbes15 <a1504533@myy.haaga-helia.fi>
2017-12-04 22:19:28 +0000

    Update idea.js

--- src/models/idea.js
+++ src/models/idea.js
@@ -4,14 +4,14 @@ const ideaFields = ['id', 'title', 'description', 'budget', 'isReadyForComments'
 'creationDate', 'lastModified', 'categoryId'];
 
 export const dbGetIdeas = () =>
-  knex('idea').select(ideaFields);
-export const dbGetIdeasById = () =>
-  knex('idea', 'id').select(ideaFields);
+  knex.select('idea').from(idea);
+export const dbGetIdeasById = (id) =>
+  knex('idea', 'id').select(idea);
 export const dbGetIdeasCommentById = () =>
-  knex('idea', 'id', 'isReadyForComments').select(ideaFields);
+  knex('idea', 'id', 'isReadyForComments').select(idea);
 export const dbDeleteIdeasById = () =>
   knex('ideaFields).where( id, 'id');
 export const dbGetIdeasCommentById = () =>
-  knex('idea', 'id', 'isReadyForComments').select(ideaFields);
+  knex('idea', 'id', 'isReadyForComments').select(idea);
 export const dbGetIdeasCommentById = () =>
-  knex('idea', 'id', 'isReadyForComments').select(ideaFields);
+  knex('idea', 'id', 'isReadyForComments').select(idea);

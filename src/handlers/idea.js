16a2cddc78e9ad163748042c79b1c86b1340a1b1
JForbes15 <a1504533@myy.haaga-helia.fi>
2017-12-05 13:37:14 +0000

    Update idea.js

--- src/handlers/idea.js
+++ src/handlers/idea.js
@@ -3,7 +3,7 @@ import {
 } from '../models/idea';
 
 export const getIdeas = (req, res) => dbGetIdeas().then(data => res.json(data));
-export const getIdeasById = (req, res) => dbGetIdeasId().then(data => res.json(data));
+export const getIdeasById = (req, res) => dbGetIdeasId(req.param.id).then(data => res.json(data));
 export const postIdeas = (req, res) => dbPostIdeas().then(data => res.json(data));
 export const deleteIdeasById = (req, res) => dbDeleteIdeasById().then(data => res.json(data));
 export const getIdeasCommentById = (req, res) => dbGetIdeasCommentById().then(data => res.json(data));

c047eea26c1abae5b5a097c56e208189dc27806f
JForbes15 <a1504533@myy.haaga-helia.fi>
2017-12-05 13:46:52 +0000

    Update idea.js

--- src/handlers/idea.js
+++ src/handlers/idea.js
@@ -5,6 +5,6 @@ import {
 export const getIdeas = (req, res) => dbGetIdeas().then(data => res.json(data));
 export const getIdeasById = (req, res) => dbGetIdeasId(req.param.id).then(data => res.json(data));
 export const postIdeas = (req, res) => dbPostIdeas().then(data => res.json(data));
-export const deleteIdeasById = (req, res) => dbDeleteIdeasById().then(data => res.json(data));
-export const getIdeasCommentById = (req, res) => dbGetIdeasCommentById().then(data => res.json(data));
-export const putIdeasById = (req, res) => dbPutIdeasById().then(data => res.json(data));
+export const deleteIdeasById = (req, res) => dbDeleteIdeasById(req.param.id).then(data => res.json(data));
+export const getIdeasCommentById = (req, res) => dbGetIdeasCommentById(req.param.id).then(data => res.json(data));
+export const putIdeasById = (req, res) => dbPutIdeasById(req.param.id).then(data => res.json(data));

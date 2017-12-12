804ca7ce89eab58114b07909e9c56ba5e0408df6
Thu Nghiem <richardsnyderyp@gmail.com>
2017-12-12 19:36:29 +0000

    remove exlude

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   module: {
     loaders: [{
-      exclude: /node_modules/,
+      //exclude: /node_modules/,
       loader: 'babel',
       query: {
         presets: ['react', 'es2015', 'stage-1']

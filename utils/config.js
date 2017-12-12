cc7678990c67c4bfab8af2ebde4a0c8ad5b3a1a1
Thu Nghiem <richardsnyderyp@gmail.com>
2017-12-12 20:57:59 +0000

    knex connection

--- utils/config.js
+++ utils/config.js
@@ -2,13 +2,8 @@ const config = {
   db: {
     // Common config for all db environments
     debug: true, // Toggle db debugging
-    client: 'pg',
-    connection: process.env.DATABASE_URL || {
-      user: 'postgres',
-      password: '',
-      database: 'postgres://iumbojeihyvvrr:51ec8c86ec8fa6c8827e1c30982c263fbfd684e5dd27b37ab926214d7c9aa024@ec2-54-243-39-245.compute-1.amazonaws.com:5432/d8680iv0mr6sdb',
-      ssl: false,
-    },
+    client: 'postgresql',
+    connection: 'postgres://iumbojeihyvvrr:51ec8c86ec8fa6c8827e1c30982c263fbfd684e5dd27b37ab926214d7c9aa024@ec2-54-243-39-245.compute-1.amazonaws.com:5432/d8680iv0mr6sdb',
     pool: {
       min: 1,
       max: 1,

3b625e9a09957a41fc33f0614bdee0dbbff8ad5d
Thu Nghiem <richardsnyderyp@gmail.com>
2017-12-14 18:39:43 +0000

    Allow Cor

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express         = require("express"), 
     app             = express(),
     cookieParser    = require("cookie-parser"),
-    mongoose        = require("mongoose"),
     bodyParser      = require("body-parser");
 
 var index           = require("./src/routes/index");
@@ -9,7 +8,11 @@ var index           = require("./src/routes/index");
 app.use(require('body-parser').urlencoded({ extended: false }));
 app.use(require('body-parser').json());
 
-mongoose.Promise = require('bluebird');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use("", index);
 

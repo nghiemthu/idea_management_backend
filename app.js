4cccc9059b7916930b3f678d622d4401c57e22f7
Thu Nghiem <richardsnyderyp@gmail.com>
2018-01-09 21:49:28 +0000

    Remove allow methods

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(require('body-parser').json());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
